79eb72b457c06786f9c707b267e2c8a33e5fac16
Jason Cartwright <jason.cartwright44@gmail.com>
2019-01-05 17:34:01 -0800

    started working

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var wins = 0
+var losses = 0
+var guessesLeft = 9
+var guessesUsed = []
+
+document.onkeyup = function(event) {
+    var guessed = event.key;
+    var answer = letters[Math.floor(Math.random() * letters.length)];
+
+
+
+
+}
